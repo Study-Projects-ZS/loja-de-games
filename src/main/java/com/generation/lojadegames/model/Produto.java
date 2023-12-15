d9359b6204ed2f8c5f7841ae835f868da82a1f0c
Wallysson Araujo <Cerberus74@live.com>
2023-12-14 23:52:47 -0300

    Relacionamento Produto

--- src/main/java/com/generation/lojadegames/model/Produto.java
+++ src/main/java/com/generation/lojadegames/model/Produto.java
@@ -1,10 +1,13 @@
 package com.generation.lojadegames.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -27,6 +30,10 @@ public class Produto {
 	@Column(length=1000)
 	@Size(max=1000, message = "O atríbuto foto pode ter no máximo 1000 caracteres.")
 	private String foto;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
 	
 	public Long getId() {
 		return id;
