9c70933857fa4d01c505e895bbe614bfcdb533ad
als-samara <samaraalmeida379@gmail.com>
2023-12-21 20:31:53 -0300

    Correção de import

--- src/main/java/com/generation/lojadegames/service/UsuarioService.java
+++ src/main/java/com/generation/lojadegames/service/UsuarioService.java
@@ -2,7 +2,7 @@ package com.generation.lojadegames.service;
 
 import java.util.Optional;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
